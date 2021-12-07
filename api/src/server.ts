18249fe038f0f0cca56c0188687796deadd0f375
Scott Dougherty <scottdougherty@Ascendums-MacBook-Pro.local>
2021-12-07 11:44:17 -0500

    Added post route for profit. Added logic in route to calculate max profit from the array

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,17 +1,30 @@
 import Koa from 'koa';
 import Router from 'koa-router';
+import bodyParser from 'koa-bodyparser';
 
 const app = new Koa();
 const router = new Router();
 
-router.get('Maximum Profit', '/', (ctx) => {
+router.get('/', (ctx) => {
   ctx.body = 'Hello World';
 });
 
-router.get('Maximum Profit', '/profit', (ctx) => {
-  ctx.body = 'Top Profit: ';
+router.post('/profit', (ctx, next) => {
+  let maximumProfit = 0;
+  const optionsPrices = ctx.request.body.values;
+  for (let i = 0; i < optionsPrices.length - 1; i++) {
+    for (let j = i + 1; j < optionsPrices.length; j++) {
+      const profit = optionsPrices[j] - optionsPrices[i];
+      if (optionsPrices[j] - optionsPrices[i] > maximumProfit) {
+        maximumProfit = profit;
+      }
+    }
+  }
+  ctx.body = maximumProfit;
 });
 
-app.use(router.routes()).use(router.allowedMethods());
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 app.listen('5000');
